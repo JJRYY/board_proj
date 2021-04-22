647c40e449205c28bef736cac95d5bd2d55a86df
Ryong <kjr2436@gmail.com>
2021-04-22 11:16:35 +0900

    command pattern

--- src/main/java/board_proj/controller/BoardFrontController.java
+++ src/main/java/board_proj/controller/BoardFrontController.java
@@ -1,32 +1,64 @@
 package board_proj.controller;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Properties;
 
-import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import board_proj.action.Action;
-import board_proj.action.BoardDeleteFormAction;
-import board_proj.action.BoardDeleteProAction;
-import board_proj.action.BoardDetailAction;
-import board_proj.action.BoardFileDownAction;
-import board_proj.action.BoardListAction;
-import board_proj.action.BoardModifyFormAction;
-import board_proj.action.BoardModifyProAction;
-import board_proj.action.BoardReplyFormAction;
-import board_proj.action.BoardReplyProAction;
-import board_proj.action.BoardWriteProAction;
-import board_proj.action.BoardWriteFormAction;
+import board_proj.action.NullAction;
 import board_proj.dto.ActionForward;
 
-@WebServlet("*.do")
+@WebServlet(urlPatterns = {"*.do"},
+			loadOnStartup = 1,
+			initParams = {@WebInitParam(name="configFile", value="/WEB-INF/commandAction.properties")}
+		)
 public class BoardFrontController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private Map<String, Action> actionMap = new HashMap();
 	
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("init() - config >> " + config.getInitParameter("configFile"));
+		String configFile = config.getInitParameter("configFile");
+		try(InputStream is = config.getServletContext().getResourceAsStream(configFile)) {
+			Properties props = new Properties();
+			props.load(is);
+			
+//			System.out.println("props >> " + props);
+			for(Entry<Object, Object> entry : props.entrySet()) {
+//				System.out.println(entry.getKey() + " : " + entry.getValue());
+				Class<?> cls;
+				Action action = null;
+				try {
+					// properties 내의 클래스명에 오타가 있을경우 어디서 발생했는지 알기 쉽게 처리
+					cls = Class.forName((String) entry.getValue());
+					action = (Action) cls.newInstance();
+				} catch(ClassNotFoundException e) {
+					action = new NullAction();
+					e.printStackTrace();
+				}
+				actionMap.put((String)entry.getKey(), action);
+			}
+			
+//			for(Entry<String, Action> entry : actionMap.entrySet()) {
+//				System.out.println(entry.getKey() + " : " + entry.getValue());
+//			}
+		} catch (IOException | InstantiationException | IllegalAccessException e) {
+			e.printStackTrace();
+		}
+	}
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doProcess(request, response);
 	}
@@ -45,8 +77,19 @@ public class BoardFrontController extends HttpServlet {
 		System.out.println(command);
 		
 		ActionForward forward = null;
-		Action action = null;
+//		Action action = null;
+		
+		Action action = actionMap.get(command);
+				
 		try {
+			forward = action.execute(request, response);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+/*
+		try {
+			
 			if (command.equals("/boardWriteForm.do")) {
 				action = new BoardWriteFormAction();
 				forward = action.execute(request, response);
@@ -87,7 +130,7 @@ public class BoardFrontController extends HttpServlet {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+*/
 		
 		if (forward != null) {
 			if(forward.isRedirect()) {
